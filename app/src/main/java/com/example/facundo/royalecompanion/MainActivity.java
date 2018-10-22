0478e166b15b97b54f6a3a9403572bdbf3932575
facundop <facundo.precentado@mercadolibre.com>
2018-10-22 14:37:21 -0300

    Add bottom menu

--- app/src/main/java/com/example/facundo/royalecompanion/MainActivity.java
+++ app/src/main/java/com/example/facundo/royalecompanion/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Button mButtonGetUpcomingChests;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +20,19 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_profile:
+                    mTextMessage.setVisibility(View.VISIBLE);
+                    mTextMessage.setText(R.string.title_profile);
+                    mButtonGetUpcomingChests.setVisibility(View.GONE);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_chests:
+                    mTextMessage.setVisibility(View.GONE);
+                    mButtonGetUpcomingChests.setVisibility(View.VISIBLE);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_settings:
+                    mTextMessage.setVisibility(View.VISIBLE);
+                    mTextMessage.setText(R.string.title_settings);
+                    mButtonGetUpcomingChests.setVisibility(View.GONE);
                     return true;
             }
             return false;
@@ -37,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        mButtonGetUpcomingChests = (Button) findViewById(R.id.get_upcoming_chests_button);
+        mButtonGetUpcomingChests.setVisibility(View.GONE);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
